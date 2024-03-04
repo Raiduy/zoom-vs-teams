f3379a31c7864de204e44ce12d5176233463aafa
Radu Apsan <radu.apsan@gmail.com>
2024-03-04 19:12:26 +0100

    added pcm functionality, behaviour still very flaky

--- zoom_vs_teams.ps1
+++ zoom_vs_teams.ps1
@@ -1,8 +1,9 @@
 $conference_tool_paths = '.\conftool_paths.secret'
 $number_of_runs = 1
-$experiment_length = 10 # in minutes
-$cooldown_length = 5 # in minutes
+$experiment_length = 60 # in seconds
+$cooldown_length = 30 # in seconds
 $variation = 'default'
+$sample_interval = 1 # in seconds
 
 function GetConfToolPath([string]$conf_tool) {
     $line = (Get-Content $conference_tool_paths | Select-String -Pattern $conf_tool)
@@ -15,41 +16,70 @@ function GetConfToolLink([string]$conf_tool) {
   return $link
 }
 
+try {
+  $experiment_folder = '.\experiments\'
+  New-Item $experiment_folder -ItemType Directory -Force | Out-Null
 
-for ($i = 0; $i -lt $number_of_runs; $i++) {
-  foreach ($tool in @('Zoom', 'Teams')) {
-    # Create a folder for the experiment
-    $experiment_folder = '.\experiments\' + (Get-Date -Format "yyyy-MM-dd_HH-mm-ss") + '_' + $tool + "_" + $variation
-    New-Item $experiment_folder -ItemType Directory -Force | Out-Null
+  # echo $experiment_length
 
-    # Open link from shell https://debug.to/696/open-url-in-browser-in-powershell
-    $link = '"' + (GetConfToolLink $tool) + '"'
-    Start-Process $link
-    
-    Write-Output "Experiment $i $tool started"
+  # PCM Sanity check
+  pcm $sample_interval -i=60 -csv=".\experiments\pcm_sanity.log"
 
-    # # Wait for the conference tool to start
-    Start-Sleep -Seconds 15
+  # pcm running idle
+  pcm $sample_interval -i=60 -csv=".\experiments\pcm_idle.log"
 
-    if ($tool -eq 'Teams') {
-      python .\teams_join.py
+  for ($i = 0; $i -lt $number_of_runs; $i++) {
+    foreach ($tool in @('Zoom', 'Teams')) {
+      # Create a folder for the experiment
+      $experiment_folder = '.\experiments\' + (Get-Date -Format "yyyy-MM-dd_HH-mm-ss") + '_' + $tool + "_" + $variation
+      New-Item $experiment_folder -ItemType Directory -Force | Out-Null
+
+      # Open link from shell https://debug.to/696/open-url-in-browser-in-powershell
+      $link = (GetConfToolLink $tool)
+      Start-Process "$link"
+      
+      Write-Output "Experiment $i $tool started"
+
+      # Wait for the conference tool to start
+      Start-Sleep -Seconds 20
+
+      if ($tool -eq 'Teams') {
+        python .\teams_join.py
+      }
+
+      # Start Intel PCM
+      pcm $sample_interval -i=60 -csv="$experiment_folder\pcm.log"
+      
+      # # Start the experiment
+      # Start-Sleep -Seconds ($experiment_length * 60)
+
+      # # Stop Intel PCM
+      # Stop-Job $pcm_job
+      
+      # # Close the conference tool
+      $path = GetConfToolPath $tool
+      $exe = $path -split '\\' | Select-Object -Last 1
+      taskkill /F /T /IM $exe
+      
+      
+      # Get Tool version
+      (Get-Item -LiteralPath $path).VersionInfo | Format-List * -force > $experiment_folder\version.log
+      
+      Write-Output "Experiment $i $tool ended"
+      Start-Sleep -Seconds $cooldown_length
     }
-
-    
-    # # Start the experiment
-    # Start-Sleep -Seconds ($experiment_length * 60)
-    
-    # # Close the conference tool
-    $path = GetConfToolPath $tool
-    $exe = $path -split '\\' | Select-Object -Last 1
-    taskkill /F /T /IM $exe
-    
-    
-    # Get Tool version
-    (Get-Item -LiteralPath $path).VersionInfo | Format-List * -force > $experiment_folder\version.log
-    
-    Write-Output "Experiment $i $tool ended"
-    Start-Sleep -Seconds 5
   }
+} finally {
+  # Close pcm
+  Stop-Job $pcm_job
+
+  # Close the conference tool
+  $path = GetConfToolPath 'Zoom'
+  $exe = $path -split '\\' | Select-Object -Last 1
+  taskkill /F /T /IM $exe
+  $path = GetConfToolPath 'Teams'
+  $exe = $path -split '\\' | Select-Object -Last 1
+  taskkill /F /T /IM $exe
+  Write-Output "Experiment ended"
 }
 
